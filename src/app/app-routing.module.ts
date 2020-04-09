9ec23e69ba37b46546aa7c8514d64dd44f05c529
carolineiordache <caroline.iordache@gmail.com>
2020-04-09 18:44:44 +0200

    Add steps

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {Step1Component} from "./step1/step1.component";
+import {Step2Component} from "./step2/step2.component";
+import {Step3Component} from "./step3/step3.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: Step1Component},
+  {path: 'step2', component: Step2Component},
+  {path: 'step3', component: Step3Component},
+  {path: '**', component: Step1Component}
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Step1Component } from './step1/step1.component';
+import { Step2Component } from './step2/step2.component';
+import { Step3Component } from './step3/step3.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Step1Component,
+    Step2Component,
+    Step3Component
   ],
   imports: [
     BrowserModule,
