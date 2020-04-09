13d0ebaf46017b27c3f43b1e65b3abbd567a2b05
carolineiordache <caroline.iordache@gmail.com>
2020-04-09 19:08:51 +0200

    Add Step4 to test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import {Routes, RouterModule} from '@angular/router';
 import {Step1Component} from "./step1/step1.component";
 import {Step2Component} from "./step2/step2.component";
 import {Step3Component} from "./step3/step3.component";
+import {Step4Component} from "./step4/step4.component";
 
 
 const routes: Routes = [
   {path: '', component: Step1Component},
   {path: 'step2', component: Step2Component},
   {path: 'step3', component: Step3Component},
+  {path: 'step4', component: Step4Component},
   {path: '**', component: Step1Component}
 ];
 
